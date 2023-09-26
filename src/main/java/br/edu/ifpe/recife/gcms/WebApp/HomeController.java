ecfac5abf8a9523e73c09e54599481664286002f
flaviovieira1 <vieiraf2@hotmail.com>
2023-09-26 17:52:01 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,20 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, world!";
+	}
+	
+	@RequestMapping("/flavio")
+	public @ResponseBody String greetingFlavio() {
+		return "Hello, Flavio!";
+	}
+
+}
